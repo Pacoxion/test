609b8a930c7561e258c999493e0c4005c43f3d7e
pacoxiong <1378308581@qq.com>
2018-11-14 19:16:42 +0800

    first co

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 window.onload=function(){
-  
+    var topbar=document.getElementsByClassName('topBar')[0].getElementsByTagName('li');
+    var roll1= document.getElementsByClassName('carousel1')[0].getElementsByTagName('img');
+    var circle= document.getElementsByClassName('cicle')[0].getElementsByTagName('div');
+    var roll2= document.getElementsByClassName('proCenterImg')[0].getElementsByTagName('img');
+    var proli= document.getElementsByClassName('proBar')[0].getElementsByTagName('li');
+
+    for(var i=0;i < topbar.length;i++){
+        topbar[i].addEventListener('mouseenter',function(e){
+            e.target.style.backgroundColor='rgb(47,90,155)';
+        });
+        topbar[i].addEventListener('mouseleave',function(e){
+            e.target.style.backgroundColor='rgb(98,134,160)';
+        });
+    }
 };
