781e94be755afcf8979884a0c2f5b0862c24a6d6
pacoxiong <1378308581@qq.com>
2018-11-14 21:07:15 +0800

    first co

--- js/main.js
+++ js/main.js
@@ -1,16 +1,61 @@
-window.onload=function(){
-    var topbar=document.getElementsByClassName('topBar')[0].getElementsByTagName('li');
-    var roll1= document.getElementsByClassName('carousel1')[0].getElementsByTagName('img');
-    var circle= document.getElementsByClassName('cicle')[0].getElementsByTagName('div');
-    var roll2= document.getElementsByClassName('proCenterImg')[0].getElementsByTagName('img');
-    var proli= document.getElementsByClassName('proBar')[0].getElementsByTagName('li');
+window.onload = function () {
 
-    for(var i=0;i < topbar.length;i++){
-        topbar[i].addEventListener('mouseenter',function(e){
-            e.target.style.backgroundColor='rgb(47,90,155)';
+    var topbar = document.getElementsByClassName('topBar')[0].getElementsByTagName('li');
+    var roll1 = document.getElementsByClassName('carousel1')[0].getElementsByTagName('img');
+    var circle = document.getElementsByClassName('cicle')[0].getElementsByTagName('div');
+    var roll2 = document.getElementsByClassName('proCenterImg')[0].getElementsByTagName('img');
+    var proli = document.getElementsByClassName('proBar')[0].getElementsByTagName('li');
+
+    //导航栏鼠标滑过
+    for (var i = 0; i < topbar.length; i++) {
+        topbar[i].addEventListener('mouseenter', function (e) {
+            e.target.style.backgroundColor = 'rgb(47,90,155)';
         });
-        topbar[i].addEventListener('mouseleave',function(e){
-            e.target.style.backgroundColor='rgb(98,134,160)';
+        topbar[i].addEventListener('mouseleave', function (e) {
+            e.target.style.backgroundColor = 'rgb(98,134,160)';
         });
     }
+
+    //轮播图1
+    function change1() {
+        if (roll1[0].style.display == 'block') {
+            roll1[0].style.display = 'none';
+            roll1[1].style.display = 'block';
+            circle[0].style.backgroundColor = 'rgb(117, 157, 186)';
+            circle[2].style.backgroundColor = '';
+        } else if (roll1[1].style.display == 'block') {
+            roll1[1].style.display = 'none';
+            roll1[2].style.display = 'block';
+            circle[1].style.backgroundColor = 'rgb(117, 157, 186)';
+            circle[0].style.backgroundColor = '';
+        } else {
+            roll1[2].style.display = 'none';
+            roll1[0].style.display = 'block';
+            circle[2].style.backgroundColor = 'rgb(117, 157, 186)';
+            circle[1].style.backgroundColor = '';
+        }
+    };
+    setInterval(change1, 3000);
+
+    //轮播图2
+    function change2() {
+        var j;
+        for (var i = 0; i < roll2.length; i++) {
+            if (roll2[i].style.display == 'block') {
+                j=i;
+            }
+        }
+        roll2[j].style.display='none';
+        proli[j].style.backgroundColor='rgb(221, 218, 218)';
+        if(j==roll2.length-1){
+            roll2[0].style.display='block';
+            proli[0].style.backgroundColor='rgb(0,104,177)';
+        }else{
+            j+=1;
+            roll2[j].style.display='block';
+            proli[j].style.backgroundColor='rgb(0,104,177)';
+        }
+    }
+    setInterval(change2, 3000);
+
 };
