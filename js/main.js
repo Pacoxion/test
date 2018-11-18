d82a018966ed8c8f2f3e5c5159d19b956009c3f5
pacoxiong <1378308581@qq.com>
2018-11-18 13:51:15 +0800

    first co

--- js/main.js
+++ js/main.js
@@ -5,21 +5,24 @@ window.onload = function () {
     var circle = document.getElementsByClassName('cicle')[0].getElementsByTagName('div');
     var roll2 = document.getElementsByClassName('proCenterImg')[0].getElementsByTagName('img');
     var proli = document.getElementsByClassName('proBar')[0].getElementsByTagName('li');
-    var foot=document.getElementById('foot');
+    var foot = document.getElementById('foot');
+    var part = document.getElementsByClassName('part')
 
     //导航栏鼠标滑过
     for (var i = 0; i < topbar.length; i++) {
         topbar[i].addEventListener('mouseenter', function (e) {
-            e.target.style.backgroundColor = 'rgb(47,90,155)';
+            //e.target.style.backgroundColor = 'rgb(47,90,155)';
+            e.target.setAttribute('id', 'tobarli');
         });
         topbar[i].addEventListener('mouseleave', function (e) {
-            e.target.style.backgroundColor = 'rgb(98,134,160)';
+            // e.target.style.backgroundColor = 'rgb(98,134,160)';
+            e.target.removeAttribute('id', 'tobarli');
         });
     }
     //联系我们滑动
-    topbar[4].addEventListener('click',function(){
-        
-        window.scrollTo(0,foot.offsetTop);
+    topbar[4].addEventListener('click', function () {
+
+        window.scrollTo(0, foot.offsetTop);
     })
 
     //轮播图1
@@ -48,18 +51,18 @@ window.onload = function () {
         var j;
         for (var i = 0; i < roll2.length; i++) {
             if (roll2[i].style.display == 'block') {
-                j=i;
+                j = i;
             }
         }
-        roll2[j].style.display='none';
-        proli[j].style.backgroundColor='rgb(221, 218, 218)';
-        if(j==roll2.length-1){
-            roll2[0].style.display='block';
-            proli[0].style.backgroundColor='rgb(0,104,177)';
-        }else{
-            j+=1;
-            roll2[j].style.display='block';
-            proli[j].style.backgroundColor='rgb(0,104,177)';
+        roll2[j].style.display = 'none';
+        proli[j].style.backgroundColor = 'rgb(221, 218, 218)';
+        if (j == roll2.length - 1) {
+            roll2[0].style.display = 'block';
+            proli[0].style.backgroundColor = 'rgb(0,104,177)';
+        } else {
+            j += 1;
+            roll2[j].style.display = 'block';
+            proli[j].style.backgroundColor = 'rgb(0,104,177)';
         }
     }
     setInterval(change2, 3000);
